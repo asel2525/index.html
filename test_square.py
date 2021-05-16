9f4a815d612e77005edd0c3a8c106d77dbf1e865
Asel Alybaeva <75333569+asel2525@users.noreply.github.com>
2021-05-16 16:23:00 +0600

    Add files via upload

---
+++ test_square.py
@@ -0,0 +1,47 @@
+# import unittest
+# from square import area
+
+# class AreaTest(unittest, TestCase):
+#     def test_correct_arguments(self):
+#         self.assertEqual(area(5, 10), 50)
+
+#     def test_incorrect_arguments(self):
+#         with self.assertRaises(ValueError):
+#             area("five", [10, 5])
+
+    
+
+
+# a = [13, 'hello', (3, 5), [8, ], 903, 5.9]
+
+# def func(a):
+#     sum = 0
+#     for i in a:
+#         try:
+#             sum += i
+#         except:
+#             continue
+#     return sum
+
+# print(func(a))
+
+
+
+class Street:
+    def __init__(self):
+        self.list_of_yards = []
+        self.amount_of_yards = 0
+
+    def func(self, yard):
+        self.list_of_yards = list_of_yards
+        
+
+class Yard:
+    name = "asd"
+
+    
+
+
+    
+
+
