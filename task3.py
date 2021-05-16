7ea8cfd1d1be47fba33d7f7123e57e98bf585939
Asel Alybaeva <75333569+asel2525@users.noreply.github.com>
2021-05-16 16:18:05 +0600

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,82 @@
+import random
+students = [
+    {'name': 'John', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Andrew', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Marcus', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Agness', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Don', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Michael', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Jennifer', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Angela', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Eniston', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Albert', 'age': 33, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nash', 'age': 28, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Nicolas', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Alexey', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Martin', 'age': 22, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Gloria', 'age': 23, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Mikel', 'age': 24, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Susanne', 'age': 25, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Stevie', 'age': 26, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Mark', 'age': 18, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Johnathan', 'age': 15, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aidin', 'age': 20, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Mirbek', 'age': 25, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aidana', 'age': 40, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Atay', 'age': 42, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Chyngyz', 'age': 17, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Aigerim', 'age': 16, 'course': 'java', 'gender': 'Female'},
+    {'name': 'Jyldyz', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Emir', 'age': 34, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Emirlan', 'age': 12, 'course': 'javascript', 'gender': 'Male'},
+    {'name': 'Nursultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Aliaskar', 'age': 19, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Aktanbek', 'age': 21, 'course': 'java', 'gender': 'Male'},
+    {'name': 'Adyl', 'age': 14, 'course': 'python', 'gender': 'Male'},
+    {'name': 'Janyl', 'age': 16, 'course': 'python', 'gender': 'Female'},
+    {'name': 'Meerim', 'age': 12, 'course': 'javascript', 'gender': 'Female'},
+    {'name': 'Sultan', 'age': 13, 'course': 'python', 'gender': 'Male'},
+]
+
+lst1 = {}
+lst2 = {}   
+lst3 = {}
+for j in students:
+    for i in j.keys():
+        if i == 'python':
+            lst1.update(i)
+        elif i == 'java':
+            lst2.update(i)
+        elif i == 'javascript':
+            lst3.update(i)
+
+
+ls = ['python']
+ls1 = ['java']
+ls2 = ['javascript']
+courses1 =  dict(zip(ls, lst1))
+courses2 =  dict(zip(ls1, lst2))
+courses3 =  dict(zip(ls2, lst3))
+
+# 2
+name = ['Asl',  'sefesf',  'fegfsrg',  'getrweg', ]
+age = [16, 23, 18, 99]
+c = dict(zip(name, age))
+# print(c)
+
+# 3
+names = ['Janyl', 'Nursultan', 'Meerim', 'Emir', 'Susann', 'Marcus', 'Aidin', 
+'Aigerim', 'Angela', 'Albert', 'Jyldyz', 'Doe', 'Gloria', 'Aliaskar',
+ 'Martin', 'John', 'Andrew', 'Steve', 'Johnathan', 'Adyl', 'Chyngyz', 
+'Michael', 'Atay', 'Mikkel', 'Agnes', 'Aidana', 'Sultan', 'Nash',
+ 'Nicolas', 'Mirbek', 'Aktan', 'Emirlan', 'Jennifer', 'Eniston', 'Alex', 'Mark']
+rans = random.random()
+names_ages = {y: rans for y in names}
+
+print(names_ages)
+
+def f(names):
+    try:
+        print(f'{names_ages.keys()} - {names_ages.values()}')
+    except:
+        print('This key isn\'t exist')
